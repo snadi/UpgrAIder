031f168af12495a197da87db58279b2a592ccf2b
Sarah Nadi <nadi@ualberta.ca>
2024-07-08 14:46:50 +0300

    Update code to working code

--- src/upgraider/fix_lib_examples.py
+++ src/upgraider/fix_lib_examples.py
@@ -1,179 +1,224 @@
-import argparse
+import os
+import difflib
+import ast
+from collections import namedtuple
+from enum import Enum
+from upgraider.Model import ModelResponse, Model, parse_model_response
+from apiexploration.Library import CodeSnippet, Library
+from upgraider.promptCrafting import construct_fixing_prompt
+from upgraider.run_code import run_code
 from upgraider.Report import (
-    Report,
     SnippetReport,
     UpdateStatus,
     RunResult,
     FixStatus,
-    DBSource,
 )
-from upgraider.run_code import run_code
-from upgraider.Model import Model, parse_model_response
-from upgraider.promptCrafting import construct_fixing_prompt
-import os
-import json
-import difflib
-from apiexploration.Library import Library
-from enum import Enum
-import ast
-from collections import namedtuple, defaultdict
-import time
 
 
-def fix_example(
-    library: Library,
-    example_file: str,
-    examples_path: str,
-    requirements_file: str,
-    output_dir: str,
-    use_references: bool,
-    model: Model,
-    threshold: float = None,
-):
-
-    example_file_path = os.path.join(examples_path, example_file)
-
-    print(f"Fixing {example_file_path}...")
-    with open(example_file_path, "r") as f:
-        original_code = f.read()
-
-    original_code_result = run_code(library, example_file_path, requirements_file)
-
-    prompt_text = construct_fixing_prompt(
-        original_code=original_code, use_references=use_references, threshold=threshold
-    )
-
-    print("Writing prompt to file...")
-    prompt_file = _write_result(
-        prompt_text, ResultType.PROMPT, output_dir, example_file
-    )
-
-    model_response = model.query(prompt_text)
-
-    print("Writing model response to file...")
-    model_response_file = _write_result(
-        model_response, ResultType.RESPONSE, output_dir, example_file
-    )
-
-    parsed_model_response = parse_model_response(model_response)
-
-    final_code_result = None  # will stay as None if no update occurs
-    updated_code_file = None
-    diff = None
-    updated_code = None
-    example_file_root = os.path.splitext(example_file)[0]
-
-    if parsed_model_response.update_status == UpdateStatus.UPDATE:
-        updated_code = parsed_model_response.updated_code
-        if updated_code is None:
-            print(
-                f"WARNING: update occurred for {example_file} but could not retrieve updated code"
-            )
-        else:
-            updated_code = _fix_imports(
-                old_code=original_code, updated_code=updated_code
-            )
-            updated_code_file = os.path.join(
-                output_dir, f"updated/{example_file_root}_updated.py"
-            )
-            os.makedirs(os.path.dirname(updated_code_file), exist_ok=True)
-            with open(updated_code_file, "w") as f:
-                f.write(updated_code)
-
-            final_code_result = run_code(library, updated_code_file, requirements_file)
-            diff = _unidiff(original_code, updated_code)
-
-    snippet_results = SnippetReport(
-        original_file=example_file,
-        api=example_file_root,  # for now, file name is in format <api>.py
-        prompt_file=prompt_file,
-        # num_references=len(parsed_model_response.references),
-        modified_file=updated_code_file,
-        original_run=original_code_result,
-        model_response=parsed_model_response,
-        model_reponse_file=model_response_file,
-        modified_run=final_code_result,
-        fix_status=(
-            _determine_fix_status(original_code_result, final_code_result)
-            if final_code_result is not None
-            else FixStatus.NOT_FIXED
-        ),
-        diff=diff,
-    )
-
-    return snippet_results
+Import = namedtuple("Import", ["module", "name", "alias"])
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Fix example(s) for a given library")
-    parser.add_argument(
-        "--libpath",
-        type=str,
-        help="absolute path of target library folder",
-        required=True,
-    )
-    parser.add_argument(
-        "--outputDir",
-        type=str,
-        help="absolute path of directory to write output to",
-        required=True,
-    )
-    parser.add_argument(
-        "--useref",
-        type=bool,
-        help="Whether to use references from documentation (i.e., augmented prompt with release notes)",
-        default=True,
-        action=argparse.BooleanOptionalAction,
-    )
-    parser.add_argument(
-        "--threshold", type=float, help="Similarity Threshold for retrieval"
-    )
-    parser.add_argument(
-        "--examplefile",
-        type=str,
-        help="Specific example file to run on (optional). Only name of example file needed.",
-        required=False,
-    )
-    parser.add_argument(
-        "--model",
-        type=str,
-        help="Which model to use for fixing",
-        default="gpt-3.5-turbo-0125",
-        choices=["gpt-3.5-turbo-0125", "gpt-4"],
-    )
+class Upgraider:
+    def __init__(self, model: Model):
+        self.model = model
 
-    args = parser.parse_args()
-    script_dir = os.path.dirname(__file__)
+    def upgraide(
+        self,
+        code_snippet: CodeSnippet,
+        library: Library,
+        use_references: bool,
+        threshold: float = 0.0,
+        output_dir: str = None,
+    ):
+        print(f"In upgraide with following arguments...")
+        print(f"code_snippet: {code_snippet}")
+        print(f"library: {library}")
+        print(f"use_references: {use_references}")
+        print(f"threshold: {threshold}")
+        print(f"output_dir: {output_dir}")
 
-    with open(os.path.join(args.libpath, "library.json"), "r") as jsonfile:
-        libinfo = json.loads(jsonfile.read())
-        library = Library(
-            name=libinfo["name"],
-            ghurl=libinfo["ghurl"],
-            baseversion=libinfo["baseversion"],
-            currentversion=libinfo["currentversion"],
-            path=args.libpath,
+        prompt_text = construct_fixing_prompt(
+            original_code=code_snippet.code,
+            use_references=use_references,
+            threshold=threshold,
         )
-        output_dir = os.path.join(script_dir, args.outputDir)
 
-        if args.examplefile is not None:
-            # fix a specific example
-            fix_example(
-                library=library,
-                example_file=args.examplefile,
-                examples_path=os.path.join(library.path, "examples"),
-                requirements_file=os.path.join(library.path, "requirements.txt"),
-                output_dir=output_dir,
-                use_references=args.useref,
-                model=args.model,
-                threshold=args.threshold,
-            )
+        print("Prompt text: ", prompt_text)
+
+        model_response = self.model.query(prompt_text)
+
+        print("Model response: ", model_response)
+
+        parsed_model_response = parse_model_response(model_response)
+        parsed_model_response.prompt = prompt_text
+        parsed_model_response.original_code = code_snippet
+        parsed_model_response.library = library
+
+        parsed_model_response.updated_code = _fix_imports(
+            old_code=parsed_model_response.original_code,
+            updated_code=parsed_model_response.updated_code,
+        )
+
+        if output_dir is not None:
+            updated_file_path = _write_updated_code(output_dir, parsed_model_response)
+            parsed_model_response.updated_code.filename = updated_file_path
+
+        return parsed_model_response
+
+    def validate_upgraide(self, model_response: ModelResponse) -> SnippetReport:
+
+        library = model_response.library
+        examples_path = os.path.join(library.path, "examples")
+        print("Examples path: ", examples_path)
+
+        if os.path.exists(examples_path):
+            requirements_file = os.path.join(library.path, "requirements.txt")
+
+            if not os.path.exists(requirements_file):
+                requirements_file = None
+
+        example_file_path = os.path.join(
+            examples_path, model_response.original_code.filename
+        )
+
+        original_code_result = run_code(library, example_file_path, requirements_file)
+
+        print("Finished validating original run....")
+
+        updated_code_result = None  # will stay as None if no update occurs
+        diff = None
+
+        if model_response.update_status == UpdateStatus.UPDATE:
+            updated_code = model_response.updated_code
+            if updated_code is None:
+                print(
+                    f"WARNING: update occurred for {model_response.original_code.filename} but could not retrieve updated code"
+                )
         else:
-            # fix all examples for this library
-            fix_lib_examples(
-                library=library,
-                output_dir=output_dir,
-                model=args.model,
-                use_references=args.useref,
-                threshold=args.threshold,
+            print("Running Updated code")
+            updated_code_result = run_code(
+                library, model_response.updated_code.filename, requirements_file
             )
+            diff = _unidiff(
+                model_response.original_code.code, model_response.updated_code.code
+            )
+
+        snippet_report = SnippetReport(
+            model_response=model_response,
+            original_run=original_code_result,
+            modified_run=updated_code_result,
+            fix_status=(
+                _determine_fix_status(original_code_result, updated_code_result)
+                if updated_code_result is not None
+                else FixStatus.NOT_FIXED
+            ),
+            diff=diff,
+        )
+
+        return snippet_report
+
+
+def _fix_imports(old_code: CodeSnippet, updated_code: CodeSnippet) -> str:
+    """
+    Given the old code and the updated code, this function will ensure that the updated code has all the imports from the old code.
+    """
+    old_imports = _get_imports(old_code.code)
+    updated_imports = _get_imports(updated_code.code)
+
+    if old_imports is None or updated_imports is None:
+        print("WARNING: could not parse imports for either old or updated code")
+        return updated_code
+
+    if updated_code.code is None or updated_code.code == "":
+        return updated_code
+
+    # if there is an old import that is not in the updated code, add it
+    for old_import in old_imports:
+        if old_import not in updated_imports:
+            updated_code.code = f"{_format_import(old_import)}\n{updated_code.code}"
+
+    return updated_code
+
+
+# GaretJax, https://stackoverflow.com/questions/9008451/python-easy-way-to-read-all-import-statements-from-py-module
+def _get_imports(code: str) -> list[Import]:
+    try:
+        ast_root = ast.parse(code)
+
+        for node in ast.iter_child_nodes(ast_root):
+            if isinstance(node, ast.Import):
+                module = []
+            elif isinstance(node, ast.ImportFrom):
+                module = node.module.split(".")
+            else:
+                continue
+
+            for n in node.names:
+                yield Import(module, n.name.split("."), n.asname)
+    except:
+        return None
+
+
+def _format_import(import_stmt: Import) -> str:
+    if import_stmt.module:
+        if import_stmt.alias is not None:
+            return f"from {'.'.join(import_stmt.module)} import {'.'.join(import_stmt.name)} as {import_stmt.alias}"
+        else:
+            return f"from {'.'.join(import_stmt.module)} import {'.'.join(import_stmt.name)}"
+    else:
+        if import_stmt.alias is not None:
+            return f"import {'.'.join(import_stmt.name)} as {import_stmt.alias}"
+        else:
+            return f"import {'.'.join(import_stmt.name)}"
+
+
+# https://stackoverflow.com/questions/845276/how-to-print-the-comparison-of-two-multiline-strings-in-unified-diff-format
+# https://stackoverflow.com/posts/845432/, Andrea Francia
+def _unidiff(old, new):
+    """
+    Helper function. Returns a string containing the unified diff of two multiline strings.
+    """
+
+    old = old.splitlines(1)
+
+    if new is not None:
+        new = new.splitlines(1)
+    else:
+        new = ""
+
+    diff = difflib.unified_diff(old, new)
+
+    return "".join(diff)
+
+
+def _determine_fix_status(
+    original_code_result: RunResult, final_code_result: RunResult
+) -> FixStatus:
+    # original status is always an error or warning
+    if final_code_result.problem_free is True:
+        return FixStatus.FIXED
+
+    if original_code_result.problem != final_code_result.problem:
+        return FixStatus.NEW_ERROR
+
+    return FixStatus.NOT_FIXED
+
+
+def _write_updated_code(output_dir: str, model_response: ModelResponse):
+    print("Writing updated code... ")
+    file_path = os.path.join(
+        output_dir,
+        f"updated/{model_response.original_code.filename}_updated.py",
+    )
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    _write_file(file_path, model_response.updated_code.code)
+    return file_path
+
+
+def _write_file(path, content):
+    if content is None:
+        content = ""
+
+    with open(path, mode="w", encoding="utf-8") as f:
+        f.write(content)
+    return path
